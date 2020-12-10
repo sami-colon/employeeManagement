36f4f63d6de5906e232229733fb1eecafcbe9489
sami-colon <abhishek.personal.email@gmail.com>
2020-12-11 01:13:00 +0530

    add employee

--- src/app/commonServices/db.service.ts
+++ src/app/commonServices/db.service.ts
@@ -23,4 +23,7 @@ export class DbService {
       }
     });
   }
+  addEmployee(emp): void {
+    this.dbData = [emp, ...this.dbData];
+  }
 }
--- src/app/employee-add/employee-add.component.ts
+++ src/app/employee-add/employee-add.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {DbService} from '../commonServices/db.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-employee-add',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeAddComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private dbService: DbService, private router: Router) { }
+  employee = {id: 0, email: '', first_name: '', last_name: '', avatar: ''};
   ngOnInit() {
   }
-
+  useDefaultImage(): void {
+    this.employee.avatar = 'assets/images/default_avatar.PNG';
+  }
+  checkValidity(fname, lname, currEmail, avatar): boolean {
+    return fname.checkValidity() && lname.checkValidity() && currEmail.checkValidity() && avatar.checkValidity();
+  }
+  addEmployee(): void {
+    this.employee.id = Date.now();
+    this.dbService.addEmployee(this.employee);
+    this.router.navigateByUrl('/employees/');
+  }
 }
