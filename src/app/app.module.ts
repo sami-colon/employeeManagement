21d5ac793b67ebac3b057a08cb1f1ce529739919
sami-colon <abhishek.personal.email@gmail.com>
2020-12-10 23:05:59 +0530

    paginator for cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainNavBarComponent } from './main-nav-bar/main-nav-bar.component';
 import { EmployeePageComponent } from './employee-page/employee-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatPaginatorModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -12,10 +14,12 @@ import { EmployeePageComponent } from './employee-page/employee-page.component';
     MainNavBarComponent,
     EmployeePageComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatPaginatorModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/employee-page/employee-page.component.ts
+++ src/app/employee-page/employee-page.component.ts
@@ -1,17 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {DbService} from '../commonServices/db.service';
+import {MatPaginator} from '@angular/material';
 
 @Component({
   selector: 'app-employee-page',
   templateUrl: './employee-page.component.html',
   styleUrls: ['./employee-page.component.css']
 })
-export class EmployeePageComponent implements OnInit {
-
+export class EmployeePageComponent implements OnInit, AfterViewInit {
+  @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
   constructor(private dbService: DbService) { }
   employeesData = [];
+  dataSource = [];
   ngOnInit() {
     this.employeesData = this.dbService.getDb();
+    this.dataSource = this.employeesData.slice(0, 8);
   }
-
+  ngAfterViewInit() {
+    this.paginator.page.subscribe(data => {
+      console.log(data, data.pageIndex, data.length);
+      this.dataSource = this.employeesData.slice(data.pageIndex * 8, data.pageIndex * 8 + 8);
+    }, error => {
+      console.log(error);
+      this.dataSource = this.employeesData.slice(0, 8);
+    });
+  }
 }
